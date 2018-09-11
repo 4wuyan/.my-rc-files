99b2cf6b9fdfb5f35db6993012f8b8affa3c8ce4
WU Yan <4wuyan@gmail.com>
2018-09-11 20:37:51 +1000

    Change the color of git branch in bash prompt.
    From dark green to light green.

--- bash.bash
+++ bash.bash
@@ -9,7 +9,7 @@ alias ls='ls --color=auto'
 parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-export PS1="\W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+export PS1="\W\[\033[92m\]\$(parse_git_branch)\[\033[00m\] $ "
 
 # Git autocomplete
 if [ -f /etc/bash_completion.d/git ]; then
