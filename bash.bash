900b97b697087a7c76a2569fee6a47e1acaa783c
WU Yan <yan.wu@iress.com>
2020-07-06 11:58:20 +1000

    small

--- bash.bash
+++ bash.bash
@@ -23,7 +23,7 @@ HISTFILESIZE=100000
 # Aliases
 # ------------
 
-alias gs='git status'
+alias g='git status'
 
 alias l='ls -CF'
 alias la='ls -A'
@@ -84,13 +84,11 @@ esac
 # Colors
 # ------------
 
-# enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
