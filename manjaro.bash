23b76bf1cb99bbd3d088fec8504d9fd135c37ab2
WU Yan <yan.wu@iress.com>
2020-07-06 16:02:05 +1000

    Remove duplicate

--- manjaro.bash
+++ manjaro.bash
@@ -2,7 +2,6 @@
 # ~/.bashrc
 #
 
-[[ $- != *i* ]] && return
 
 colors() {
 	local fgc bgc vals seq0
@@ -30,8 +29,6 @@ colors() {
 		echo; echo
 	done
 }
-
-[ -r /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
 
 # Change the window title of X terminals
 case ${TERM} in
@@ -100,18 +97,9 @@ xhost +local:root > /dev/null 2>&1
 
 complete -cf sudo
 
-# Bash won't get SIGWINCH if another process is in the foreground.
-# Enable checkwinsize so that bash will check the terminal size when
-# it regains control.  #65623
-# http://cnswww.cns.cwru.edu/~chet/bash/FAQ (E11)
-shopt -s checkwinsize
-
 shopt -s expand_aliases
 
 # export QT_SELECT=4
-
-# Enable history appending instead of overwriting.  #139609
-shopt -s histappend
 
 #
 # # ex - archive extractor
@@ -137,6 +125,3 @@ ex ()
     echo "'$1' is not a valid file"
   fi
 }
-
-alias vi=vim
-stty -ixon
