478e55c80aaecf02374e4bc925ca43c1456625ec
WU Yan <13315189+4wuyan@users.noreply.github.com>
2020-07-12 22:42:43 +1000

    add comment

--- fish.fish
+++ fish.fish
@@ -13,6 +13,10 @@ abbr --add --global l 'ls -CF'
 abbr --add --global mv "mv --interactive"  # confirm if overwriting
 abbr --add --global v 'vim'
 abbr --add --global vi 'vim'
+
+#------------------
+# WSL specific
+#------------------
 if string match --quiet '*Microsoft' < /proc/sys/kernel/osrelease
 	abbr --add --global p 'powershell.exe'
 	abbr --add --global s 'smerge.exe .'
